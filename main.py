894ac0a29973cd7fd8df87e2f52211f115c71622
Zshiteng <1148337504@qq.com>
2021-03-04 17:06:25 +0800

    second edit

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 from config import DevConfig
 app = Flask(__name__)
 app.config.from_object(DevConfig)
-@app.route('/') 
+@app.route('/')
 def home():
     return '<h1>Hello World!</h1>'
 if __name__ == '__main__':
